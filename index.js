831e4f97b795b1a95f7d7ada504ab607e75b67f8
FirzaNovianto <virza56@gmail.com>
2020-04-25 11:29:07 +0700

    try to connect

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 2021
+
+app.use(express.json())
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const URL = 'mongodb://127.0.0.1:27017'
+const database = 'api-mongo'
+
+MongoClient.connect(URL, {useNewUrlParser : true, useUnifiedTopology : true}, (err,client) => {
+    if(err){
+        return console.log('gagal konek')
+
+    }
+
+    const db = client.db(database)
+
+    app.get('/',(req,res) => {
+        res.send(
+            '<h1>welcome </h1>'
+        )
+    })
+
+ 
+
+})
+
+app.listen(port, () => {console.log('API running at port ' + port)})
